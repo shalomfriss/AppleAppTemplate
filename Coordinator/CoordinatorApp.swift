5c366a05ebbc8108a284ffa1aa5d3ccfb2ffe688
Shalom Friss <shalomfriss@gmail.com>
2025-03-01 20:59:38 -0600

    Initial Commit

---
+++ Coordinator/CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorApp.swift
+//  Coordinator
+//
+//  Created by Shalom Friss on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoordinatorTests/CoordinatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoordinatorTests.swift
+//  CoordinatorTests
+//
+//  Created by Shalom Friss on 3/1/25.
+//
+
+import Testing
+
+struct CoordinatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
